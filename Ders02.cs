2d27732edfef0d3c45fe1b569517cb86d2d827dc
ayazirfan <byirfanayaz@gmail.com>
2024-01-29 22:34:52 +0300

    if-else-02

---
+++ Ders02.cs
@@ -0,0 +1,35 @@
+﻿namespace AlgoritmaDersler;
+
+public class Ders02
+{
+
+    public static void Anakod()
+    {
+        System.Console.WriteLine("Bir karakter giriniz:");
+        char ch = (char)Console.Read();
+
+        if (Char.IsUpper(ch))
+        {
+            System.Console.WriteLine("Girilen Karakter büyük bir karakterdir.");
+        }
+        else if (Char.IsLower(ch))
+        {
+            System.Console.WriteLine("Girilen Karakter küçük bir karakterdir.");
+        }
+        else if (Char.IsDigit(ch))
+        {
+            System.Console.WriteLine("Girilen Karakter bir rakamdır.");
+        }
+        else
+        {
+            System.Console.WriteLine("Girilen Karakter Alfanumerik bir ifade değildir.");     
+        }
+
+    }
+
+
+
+
+
+
+}
