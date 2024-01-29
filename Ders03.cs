7d07b634eeb779427bd38114cde6fb09d62b2bc7
ayazirfan <byirfanayaz@gmail.com>
2024-01-29 22:35:52 +0300

    Boxing-Unboxing

---
+++ Ders03.cs
@@ -0,0 +1,15 @@
+﻿public class Ders03
+{
+    public static void Anakod()
+    {
+        int i=23;
+object o =i;
+
+// Unboxing- Kutudan çıkarma
+i*=2;
+i= (int)o;
+System.Console.WriteLine("Değer turu: {0}", i);
+System.Console.WriteLine("Referans turu: {0}",o);
+    }
+
+}
