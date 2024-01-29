57ee8ba281f24d578fb45e8e3ffb7b08c9113f7c
ayazirfan <byirfanayaz@gmail.com>
2024-01-29 22:34:02 +0300

    if-else-01

---
+++ Ders01.cs
@@ -0,0 +1,32 @@
+﻿namespace AlgoritmaDersler;
+
+public class Ders01
+{
+    public static void Anakod()
+    {
+        int m=9;
+        int n= 7;
+        int p = 5;
+
+        if (m>=n && m>=p)
+        {
+            System.Console.WriteLine("En büyük m");
+        }
+        else if (m>n && !(p>n))
+        {
+            System.Console.WriteLine("En büyük m");
+        }
+        else if (m>n || m>p)
+        {
+            System.Console.WriteLine("m Enküçük değil");    
+        }
+        m=4;
+        if (!(m>=n || m>=p))
+        {
+            System.Console.WriteLine("m artık en küçük");
+            
+        }
+    }
+
+
+}
