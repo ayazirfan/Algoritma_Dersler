adb8df57464870b2ac62791b6ebfa8539fe49d37
ayazirfan <byirfanayaz@gmail.com>
2024-01-31 13:11:49 +0300

    Asal Mı

---
+++ Ders07.cs
@@ -0,0 +1,23 @@
+﻿namespace AlgoritmaDersler;
+
+public class Ders07
+{
+    public static bool AsalMi(int n)
+    {
+        if (n<=1)
+        {
+            System.Console.WriteLine("En küçük asal sayı 2'dir. ");
+            return false;
+        }
+        bool kontrol = true;
+        for (int i = 2; i < n; i++)
+        
+            if (n%i==0)
+            {
+                kontrol = false;
+                break;
+            }
+            return kontrol;
+    }        
+
+}
---
+++ Ders08.cs
@@ -0,0 +1,22 @@
+﻿namespace AlgoritmaDersler;
+
+public class Ders08
+{
+    /// <summary>
+    /// Parametre olarak aldigi sayinin rakamlari toplamini doner
+    /// </summary>
+    /// <param name="n">sayi</param>
+    /// <returns>Rakamlarin Toplamidir.</returns>
+    public static int RakamlariToplami(int n)
+    {
+        int toplam = 0, rakam=0;
+        while (n>0)
+        {
+            rakam = n%10;
+            toplam+=rakam;
+            n = n / 10;
+        }
+        return toplam;
+    }
+
+}
