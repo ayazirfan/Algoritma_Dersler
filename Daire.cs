4f072919574cc8ea99becd5cab6cbe5193a332c5
ayazirfan <byirfanayaz@gmail.com>
2024-02-03 23:23:03 +0300

    Daire Alan

---
+++ Daire.cs
@@ -0,0 +1,29 @@
+﻿namespace AlgoritmaDersler;
+
+public class Daire
+{
+    public const double pi =3.14; 
+    /// <summary>
+    /// Daireni alanini hesaplar
+    /// </summary>
+    /// <param name="r">Yaricap</param>
+    /// <returns>Alan degeridir.</returns>
+
+public static double AlaniHesapla(double r)
+{
+return pi*r*r;
+}
+/// <summary>
+/// Dairenin Alanınınhesaplar.
+/// </summary>
+/// <param name="r">Yarıçap</param>
+/// <param name="aci">Gördüğü açı</param>
+/// <returns>Alan değeri</returns>
+public static double AlaniHesapla(double r, double aci)
+{
+return pi*r*r*aci/360;
+}
+
+
+}
+
---
+++ Ders12.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace AlgoritmaDersler;
+
+public class Ders12
+{
+    public const double pi = 3.14;
+
+    public static double Cevre (double r)
+    {
+        return 2*pi*r;
+    }
+    // double r =Convert.ToDouble(Console.ReadLine());
+    // Console.WriteLine("2*{0:F2} * {1:F3} = {2:F2}", Ders12.pi,r,Ders12.Cevre(r) );
+
+}
